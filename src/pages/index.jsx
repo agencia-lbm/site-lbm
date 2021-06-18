a3480a973b03b5ffe8d456fd478659412972f68d
mfarsetti <matheus.farsetti@hotmail.com>
2021-06-18 17:38:43 -0300

    page maintenance

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,30 @@
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 
 import HomeModules from '../modules/Home';
+import styles from './styles.module.scss';
+
 
 export default function Home() {
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+  }, [])
+
   return (
-    <div>
+    <>
+      {
+        loading ? (
+          <div className={styles.manutencaoWrapper}>
+            <img src="./manutencao.gif" alt="Manutenção LBM" />
+          </div>
+        ) : <HomeModules />
+      }
       <Head>
         <title>Agencia LBM</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <HomeModules />
-    </div>
+    </>
   );
 }
