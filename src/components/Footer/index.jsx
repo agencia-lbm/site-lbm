fce3370d8677649d5b53be19f8375c58578fd09a
mfarsetti <matheus.farsetti@gmail.com>
2021-12-20 17:45:23 -0300

    Co-authored-by: fillipog <fillipog@users.noreply.github.com>

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,28 +1,28 @@
 import styles from './Footer.module.scss';
 
 export default function Footer() {
-    return (
-        <footer className={styles.footerWrapper}>
-            <div className={styles.copyrightFooter}>
-                <p>©2021 AGÊNCIA LBM | TODOS OS DIREITOS RESERVADOS</p>
-            </div>
-            <div className={styles.socialFooter}>
-                <div className={styles.facebook}>
-                    <a href="#">
-                        <img src="/assets/images/facebook.png" alt="" />
-                    </a>
-                </div>
-                <div className={styles.instagram}>
-                    <a href="#">
-                        <img src="/assets/images/instagram.png" alt="" />
-                    </a>
-                </div>
-                <div className={styles.linkedin}>
-                    <a href="#">
-                        <img src="/assets/images/linkedin.png" alt="" />
-                    </a>
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className={styles.footerWrapper}>
+      <div className={styles.copyrightFooter}>
+        <p>©2021 AGÊNCIA LBM | TODOS OS DIREITOS RESERVADOS</p>
+      </div>
+      <div className={styles.socialFooter}>
+        <div className={styles.facebook}>
+          <a href="#">
+            <img src="/assets/images/facebook.png" alt="" />
+          </a>
+        </div>
+        <div className={styles.instagram}>
+          <a href="#">
+            <img src="/assets/images/instagram.png" alt="" />
+          </a>
+        </div>
+        <div className={styles.linkedin}>
+          <a href="#">
+            <img src="/assets/images/linkedin.png" alt="" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
